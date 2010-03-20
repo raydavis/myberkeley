d733a4e5868c48f5eca0d788b982c63ad2aba1ea
Ray Davis <ray@media.berkeley.edu>
2010-03-19 17:04:17 -0700

    Fix Groovy script; make copy-and-paste from README easier

--- app/src/main/resources/SLING-INF/initial-content/apps/myberkeley/scriptedsample/json.groovy
+++ app/src/main/resources/SLING-INF/initial-content/apps/myberkeley/scriptedsample/json.groovy
@@ -3,6 +3,6 @@ response.setCharacterEncoding("UTF-8");
 
 log.info "/apps/myberkeley/scriptedsample/json.groovy running against resource=${resource}, currentNode=${currentNode}"
 
-innards = currentNode.properties.collect{ '{"' + it.name + '":"' + it.value + '"}' }.join(",")
+innards = currentNode.properties.collect{ '{"' + it.name + '":"' + it.value.string + '"}' }.join(",")
 
 println "[" + innards + ", {thisisfrom:groovy}]"
