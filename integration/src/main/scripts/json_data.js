96c41c3cadd9ac1d4de295ade1e033aeee824bb5
John King <johnk@media.berkeley.edu>
2010-06-10 17:07:32 -0700

    completion of generating json data for project members aka defined users

--- integration/src/main/scripts/json_data.js
+++ integration/src/main/scripts/json_data.js
@@ -1,9 +1,65 @@
 {
   "users": {
-    "johnking": {
-      "firstName": "John",
+    "238382": {
+      "lastName": "Geuy",
+      "firstName": "Bernadette",
+      "email": "BernadetteG@berkeley.edu"
+    },
+    "163402": {
+      "lastName": "Scronce",
+      "firstName": "David",
+      "email": "dscronce@berkeley.edu"
+    },
+    "6576": {
+      "lastName": "Christopher",
+      "firstName": "Tony",
+      "email": "tonyc@berkeley.edu"
+    },
+    "592722": {
+      "lastName": "Ellison",
+      "firstName": "Michael"
+    },
+    "2040": {
+      "lastName": "Heyer",
+      "firstName": "Oliver",
+      "email": "oliver@media.berkeley.edu"
+    },
+    "322279": {
+      "lastName": "Cochran",
+      "firstName": "Eli",
+      "email": "eli@media.berkeley.edu"
+    },
+    "211159": {
+      "lastName": "Davis",
+      "firstName": "Ray",
+      "email": "ray@media.berkeley.edu"
+    },
+    "271592": {
       "lastName": "King",
+      "firstName": "John f",
       "email": "johnk@media.berkeley.edu"
+    },
+    "750152": {
+      "lastName": "Voytek",
+      "firstName": "Jessica b"
+    },
+    "764968": {
+      "lastName": "Lai",
+      "firstName": "Whitney"
+    },
+    "675750": {
+      "lastName": "Hollowgrass",
+      "firstName": "Rachel",
+      "email": "rhollow@berkeley.edu"
+    },
+    "313561": {
+      "lastName": "Waid",
+      "firstName": "Matthew"
+    },
+    "760063": {
+      "lastName": "Ng",
+      "firstName": "Nicole",
+      "email": "nicole.ng@berkeley.edu"
     }
   }
 }
---
+++ integration/src/main/scripts/ucb_ldap_search.rb
@@ -0,0 +1,92 @@
+#!/usr/bin/env ruby
+
+
+require 'rubygems'
+require 'net/ldap'
+require 'ucb_ldap'
+require 'json/pure'  #need to use json_pure because of bug in json/ext 1.4.3
+
+module MyBerkeleyData
+
+# utility for making defined user json data file for sling user creating in sling_data_loader.rb
+# currently sling_data_loader.rb does not call this directly but reads the output file.
+# this could be easily changed to a direct call but currently am checking in the data file for other possible uses
+
+  def make_defined_users_json
+    hits = []
+    staff = UCB::LDAP::Entry.search(:base => "ou=people,dc=berkeley,dc=edu", :filter => make_staff_filter)
+    puts "found #{staff.length} staff members"
+    hits.concat staff
+    
+    students = UCB::LDAP::Entry.search(:base => "ou=people,dc=berkeley,dc=edu", :filter => make_students_filter)
+    puts "found #{students.length} student members"
+    hits.concat students
+    
+    #hits.sort {|x, y| x.attributes[:sn][0].slice(1) <=> y.attributes[:sn][0].slice(1) }  # this appears to not work, giving up
+  
+    return make_user_json hits
+  end
+  
+  def write_json(json_data, file_name = "json_data.js")
+    json_file = File.new "json_data.js", "w"
+    json_file.write json_data
+  end
+  
+  def make_staff_filter
+    
+    rachael = make_name_filter "Rachel", "Hollowgrass"
+    tonyc = make_name_filter "Tony", "Christopher"
+    bernie = make_name_filter "Bernadette", "Geuy"
+    eli = make_name_filter "Eli", "Cochran"
+    oliver = make_name_filter "Oliver", "Heyer"
+    ray = make_name_filter "Ray", "Davis"
+    johnk = make_name_filter "John F", "King"
+    davids = make_name_filter "David", "Scronce"
+    
+    return rachael | tonyc | bernie | eli | oliver | ray | johnk | davids
+  end
+  
+  def make_students_filter
+    
+    mattheww = make_name_filter "Matthew", "Waid"
+    michaele= make_name_filter "Michael", "Ellison"
+    jessicav = make_name_filter "Jessica B", "Voytek"
+    whitneyl = make_name_filter "Whitney", "Lai"
+    nicolen = make_name_filter "Nicole", "Ng"
+    
+    return mattheww | michaele | jessicav | whitneyl | nicolen
+  end
+  
+  def make_name_filter first, last
+    ff = Net::LDAP::Filter.eq("givenname", first.capitalize) 
+    lf = Net::LDAP::Filter.eq("sn", last.upcase)
+    return ff & lf
+  end
+  
+  
+  def make_user_json(entries)
+    all_data_hash = {}
+    all_users_hash = {}
+    entries.each do |entry|
+      user_hash = {}
+      user_hash["firstName"] = entry.attributes[:givenname][0].capitalize if entry.attributes[:givenname]
+      user_hash["lastName"] = entry.attributes[:sn][0].capitalize if entry.attributes[:sn]
+      user_hash["email"] = entry.attributes[:mail][0] if entry.attributes[:mail]
+      all_users_hash[entry.attributes[:uid][0]] = user_hash
+    end
+    #all_users_hash.sort{|x, y| x[1]["lastName"] <=> y[1]["lastName"] }
+    #puts all_users_hash.inspect
+    all_data_hash["users"] = all_users_hash
+    users_json = JSON.pretty_generate all_data_hash
+    puts users_json
+    return users_json
+  end
+end
+
+if __FILE__ == $0
+  include MyBerkeleyData
+  json = MyBerkeleyData.make_defined_users_json
+  MyBerkeleyData.write_json json
+end
+
+
