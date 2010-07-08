32ecf8d4ffcdf0065b4feae417f60af9784f8fd5
John King <johnk@media.berkeley.edu>
2010-07-07 18:28:29 -0700

    fleshing out notices

---
+++ notices/src/main/java/edu/berkeley/myberkeley/api/notice/MyBerkeleyMessageConstants.java
@@ -0,0 +1,10 @@
+package edu.berkeley.myberkeley.api.notice;
+
+import org.sakaiproject.nakamura.api.message.MessageConstants;
+
+public interface MyBerkeleyMessageConstants extends MessageConstants {
+	 /**
+	   * Identifier for an a notice
+	   */
+	  public static final String TYPE_NOTICE = "notice";
+}
