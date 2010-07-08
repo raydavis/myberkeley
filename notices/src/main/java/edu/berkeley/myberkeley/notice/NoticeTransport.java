f7f1e481da691a5a833bf9c6df12a644748a5b10
John King <johnk@media.berkeley.edu>
2010-07-07 18:26:27 -0700

    fleshing out notices component

--- notices/src/main/java/edu/berkeley/myberkeley/notice/NoticeTransport.java
+++ notices/src/main/java/edu/berkeley/myberkeley/notice/NoticeTransport.java
@@ -1,17 +1,92 @@
 package edu.berkeley.myberkeley.notice;
 
 import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.scr.annotations.Services;
+import org.apache.sling.commons.json.io.JSONWriter;
+import org.apache.sling.jcr.api.SlingRepository;
 import org.osgi.service.event.Event;
+import org.sakaiproject.nakamura.api.message.MessageConstants;
+import org.sakaiproject.nakamura.api.message.MessageProfileWriter;
+import org.sakaiproject.nakamura.api.message.MessageRoute;
 import org.sakaiproject.nakamura.api.message.MessageRoutes;
 import org.sakaiproject.nakamura.api.message.MessageTransport;
+import org.sakaiproject.nakamura.api.message.MessagingService;
+import org.sakaiproject.nakamura.util.JcrUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class NoticeTransport implements MessageTransport {
+import edu.berkeley.myberkeley.api.notice.MyBerkeleyMessageConstants;
 
-	public void send(MessageRoutes routes, Event event, Node n) {
-		// TODO Auto-generated method stub
-		
-	}
+/**
+ * Handler for notices. Needs to be started immediately to make sure it
+ * registers with JCR as soon as possible.
+ */
+@Component(immediate = true, label = "NoticeTransport", description = "Handler for notices.")
+@Services(value = { @Service(value = MessageTransport.class), @Service(value = MessageProfileWriter.class) })
+@Properties(value = { @Property(name = "service.vendor", value = "University of California, Berkeley"),
+        @Property(name = "service.description", value = "Handler for notices.") })
+public class NoticeTransport implements MessageTransport, MessageProfileWriter {
+    private static final Logger LOG = LoggerFactory.getLogger(NoticeTransport.class);
 
+    @Reference
+    protected transient SlingRepository slingRepository;
 
+    @Reference
+    protected transient MessagingService messagingService;
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.sakaiproject.nakamura.api.message.MessageTransport#send(org.sakaiproject.nakamura.api.message.MessageRoutes,
+     *      org.osgi.service.event.Event, javax.jcr.Node)
+     */
+    public void send(MessageRoutes routes, Event event, Node originalNotice) {
+        try {
+            Session session = slingRepository.loginAdministrative(null);
+
+            for (MessageRoute route : routes) {
+                if (MyBerkeleyMessageConstants.TYPE_NOTICE.equals(route.getTransport())) {
+                    LOG.info("Started handling a notice.");
+                    String rcpt = route.getRcpt();
+                    // the path were we want to save messages in.
+                    String messageId = originalNotice.getProperty(MessageConstants.PROP_SAKAI_ID).getString();
+                    String toPath = messagingService.getFullPathToMessage(rcpt, messageId, session);
+
+                    // Copy the node into the user his folder.
+                    JcrUtils.deepGetOrCreateNode(session, toPath.substring(0, toPath.lastIndexOf("/")));
+                    session.save();
+                    session.getWorkspace().copy(originalNotice.getPath(), toPath);
+                    Node n = JcrUtils.deepGetOrCreateNode(session, toPath);
+
+                    // Add some extra properties on the just created node.
+                    n.setProperty(MessageConstants.PROP_SAKAI_READ, false);
+                    n.setProperty(MessageConstants.PROP_SAKAI_MESSAGEBOX, MessageConstants.BOX_INBOX);
+                    n.setProperty(MessageConstants.PROP_SAKAI_SENDSTATE, MessageConstants.STATE_NOTIFIED);
+
+                    if (session.hasPendingChanges()) {
+                        session.save();
+                    }
+                }
+            }
+        }
+        catch (RepositoryException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+
+    public void writeProfileInformation(Session session, String recipient, JSONWriter write) {
+
+    }
+
+    public String getType() {
+        return MyBerkeleyMessageConstants.TYPE_NOTICE;
+    }
 }
--- notices/src/main/java/edu/berkeley/myberkeley/notice/servlet/CreateNoticeServlet.java
+++ notices/src/main/java/edu/berkeley/myberkeley/notice/servlet/CreateNoticeServlet.java
@@ -22,38 +22,48 @@ import javax.jcr.RepositoryException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferenceCardinality;
+import org.apache.felix.scr.annotations.ReferencePolicy;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.JSONObject;
 import org.apache.sling.commons.json.jcr.JsonJcrNode;
+import org.sakaiproject.nakamura.api.message.CreateMessagePreProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@SlingServlet(methods = { "GET", "POST" }, paths = { "myberkeley/notice" }, generateComponent = true, generateService = true)
-public class CreateNoticeServlet extends SlingSafeMethodsServlet {
-	private static final long serialVersionUID = -6925414746881506489L;
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(CreateNoticeServlet.class);
+@SlingServlet(resourceTypes = { "/myberkeley/notice" }, selectors = { "create" }, methods = { "POST" }, generateComponent = true, generateService = true)
+@Properties(value = { @Property(name = "service.vendor", value = "University of California, Berkeley"),
+        @Property(name = "service.description", value = "Endpoint to create a notice") })
+@Reference(name = "createMessagePreProcessor", referenceInterface = CreateMessagePreProcessor.class, cardinality = ReferenceCardinality.OPTIONAL_MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+public class CreateNoticeServlet extends SlingAllMethodsServlet {
+    private static final long serialVersionUID = -6925414746881506489L;
 
-	@Override
-	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
-			throws ServletException, IOException {
-		Resource resource = request.getResource();
-		try {
-			JSONObject jsonObject = new JsonJcrNode(resource.adaptTo(Node.class));
-			jsonObject.put("thisisfrom", "noticesservlet");
-			jsonObject.write(response.getWriter());
-		} catch (JSONException e) {
-			LOGGER.error(e.getMessage(), e);
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
-		} catch (RepositoryException e) {
-			LOGGER.error(e.getMessage(), e);
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
-		}
-	}
-	
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreateNoticeServlet.class);
+
+    @Override
+    protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+        Resource resource = request.getResource();
+        try {
+            JSONObject jsonObject = new JsonJcrNode(resource.adaptTo(Node.class));
+            jsonObject.put("thisisfrom", "noticesservlet");
+            jsonObject.write(response.getWriter());
+        }
+        catch (JSONException e) {
+            LOGGER.error(e.getMessage(), e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+        catch (RepositoryException e) {
+            LOGGER.error(e.getMessage(), e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
 }
