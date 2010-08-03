f80394a98beb11c1255cbe805f25c30ff2cfb8c5
John King <johnk@media.berkeley.edu>
2010-08-03 16:11:20 -0700

    fixes for making sakai:dueDate of type Date in recipients' messages

--- notices/src/main/java/edu/berkeley/myberkeley/notice/NoticeHandler.java
+++ notices/src/main/java/edu/berkeley/myberkeley/notice/NoticeHandler.java
@@ -1,9 +1,18 @@
 package edu.berkeley.myberkeley.notice;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Dictionary;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
 
 import javax.jcr.Node;
 import javax.jcr.PathNotFoundException;
+import javax.jcr.PropertyType;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.Value;
@@ -14,8 +23,13 @@ import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.felix.scr.annotations.Services;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.User;
+import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.io.JSONWriter;
+import org.apache.sling.commons.osgi.OsgiUtil;
 import org.apache.sling.jcr.api.SlingRepository;
+import org.osgi.service.component.ComponentContext;
 import org.osgi.service.event.Event;
 import org.sakaiproject.nakamura.api.message.MessageConstants;
 import org.sakaiproject.nakamura.api.message.MessageProfileWriter;
@@ -23,6 +37,9 @@ import org.sakaiproject.nakamura.api.message.MessageRoute;
 import org.sakaiproject.nakamura.api.message.MessageRoutes;
 import org.sakaiproject.nakamura.api.message.MessageTransport;
 import org.sakaiproject.nakamura.api.message.MessagingService;
+import org.sakaiproject.nakamura.api.presence.PresenceService;
+import org.sakaiproject.nakamura.api.presence.PresenceUtils;
+import org.sakaiproject.nakamura.util.ExtendedJSONWriter;
 import org.sakaiproject.nakamura.util.JcrUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,6 +67,16 @@ public class NoticeHandler implements MessageTransport, MessageProfileWriter {
     @Reference
     protected transient MessagingService messagingService;
 
+    @Reference
+    protected transient MessageProfileWriter messageProfileWriter;
+    
+
+    @Property(value = { "EEE MMM dd yyyy HH:mm:ss 'GMT'Z", "yyyy-MM-dd'T'HH:mm:ss.SSSZ", "yyyy-MM-dd'T'HH:mm:ss", "yyyy-MM-dd", "dd.MM.yyyy HH:mm:ss",
+            "dd.MM.yyyy" })
+    private static final String PROP_DATE_FORMAT = "notice.dateFormats";
+
+    private List<DateFormat> dateFormats = new LinkedList<DateFormat>();
+
     /**
      * {@inheritDoc}
      * 
@@ -69,19 +96,20 @@ public class NoticeHandler implements MessageTransport, MessageProfileWriter {
                     String messageId = originalNotice.getProperty(MessageConstants.PROP_SAKAI_ID).getString();
                     String toPath = messagingService.getFullPathToMessage(rcpt, messageId, session);
 
-                    // Copy the node into the user his folder.
+                    // Copy the node into the recipient's folder.
                     JcrUtils.deepGetOrCreateNode(session, toPath.substring(0, toPath.lastIndexOf("/")));
                     session.save();
                     session.getWorkspace().copy(originalNotice.getPath(), toPath);
                     Node n = JcrUtils.deepGetOrCreateNode(session, toPath);
                     LOG.debug("created recipient mesaage node: " + n.toString());
-                    // need to due this convesion on new node
-                    convertDueDate(originalNotice, n);
+                    // need to due this conversion issue on new recipient node
+                    handleDueDate(originalNotice, n);
                     // Add some extra properties on the just created node.
                     n.setProperty(MessageConstants.PROP_SAKAI_READ, false);
                     n.setProperty(MessageConstants.PROP_SAKAI_MESSAGEBOX, MessageConstants.BOX_INBOX);
                     n.setProperty(MessageConstants.PROP_SAKAI_SENDSTATE, MessageConstants.STATE_NOTIFIED);
-                    // only put the single recipient into this property, not all of them
+                    // only put the single recipient into this property, not all
+                    // of them
                     n.setProperty(MessageConstants.PROP_SAKAI_TO, route.getRcpt());
 
                     if (session.hasPendingChanges()) {
@@ -95,17 +123,94 @@ public class NoticeHandler implements MessageTransport, MessageProfileWriter {
         }
     }
 
-    private void convertDueDate(Node originalNotice, Node newNotice) throws PathNotFoundException, RepositoryException {
-       javax.jcr.Property dueDateProp = originalNotice.getProperty(MyBerkeleyMessageConstants.PROP_SAKAI_DUEDATE);
-       Value value = dueDateProp.getValue();
-//       value.
-       Calendar senderDueDate = dueDateProp.getDate();
-       LOG.debug("setting " + MyBerkeleyMessageConstants.PROP_SAKAI_DUEDATE + " to " + senderDueDate.getTime().toString() + " in newNotice: " + newNotice.toString());
-       newNotice.setProperty(MyBerkeleyMessageConstants.PROP_SAKAI_DUEDATE, senderDueDate);
+    /**
+     * method to make sure the recipient notice node stores a Date object
+     * sometimes the originalNotice has a String and sometimes a Date at this
+     * point, assuming that's a concurrency issue with the SlingPostServlet's
+     * date parsing running before or after this method. Haven't tracked it down
+     * exactly but this logic fixes the issue making sure the recipient notice
+     * has a Date dueDate
+     * 
+     * @param originalNotice
+     * @param newNotice
+     * @throws PathNotFoundException
+     * @throws RepositoryException
+     */
+    private void handleDueDate(Node originalNotice, Node newNotice) throws PathNotFoundException, RepositoryException {
+        javax.jcr.Property dueDateProp = originalNotice.getProperty(MyBerkeleyMessageConstants.PROP_SAKAI_DUEDATE);
+        if (dueDateProp != null) {
+            LOG.debug("handleDueDate() got source dueDateProp: " + dueDateProp);
+            Value value = dueDateProp.getValue();
+            int dueDateValueType = value.getType();
+            LOG.debug("handleDueDate() got source dueDateProp Value Type: " + dueDateValueType);
+            Calendar senderDueDate = null;
+            if (PropertyType.STRING == dueDateValueType) {
+                senderDueDate = parse(value.getString());
+            }
+            else if (PropertyType.DATE == dueDateValueType) {
+                senderDueDate = dueDateProp.getDate();
+            }
+            else {
+                LOG.error("handleDueDate() sourceDueDate is not a String or Date, is PropertyType: " + dueDateValueType);
+            }
+            if (senderDueDate != null) {
+                LOG.debug("setting " + MyBerkeleyMessageConstants.PROP_SAKAI_DUEDATE + " to " + senderDueDate.getTime().toString() + " in newNotice: "
+                        + newNotice.toString());
+                newNotice.setProperty(MyBerkeleyMessageConstants.PROP_SAKAI_DUEDATE, senderDueDate);
+            }
+            else {
+                LOG.error("handleDueDate() sourceDueDate is null");
+            }
+        }
+        else {
+            LOG.info("handleDueDate() no dueDate in sender's originalNotice, perhaps it's not a Reminder?");
+        }
     }
 
-    public void writeProfileInformation(Session session, String recipient, JSONWriter write) {
+    /**
+     * copied from SlingPostServlet DateParser helper Parses the given source
+     * string and returns the respective calendar instance. If no format matches
+     * returns <code>null</code>.
+     * <p/>
+     * Note: method is synchronized because SimpleDateFormat is not.
+     * 
+     * @param source
+     *            date time source string
+     * @return calendar representation of the source or <code>null</code>
+     */
+    public synchronized Calendar parse(String source) {
+        for (DateFormat fmt : this.dateFormats) {
+            try {
+                Date d = fmt.parse(source);
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Parsed " + source + " using " + ((SimpleDateFormat) fmt).toPattern() + " into " + d);
+                }
+                Calendar c = Calendar.getInstance();
+                c.setTime(d);
+                return c;
+            }
+            catch (ParseException e) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Failed parsing " + source + " using " + ((SimpleDateFormat) fmt).toPattern());
+                }
+            }
+        }
+        return null;
+    }
 
+    public String getType() {
+        return MyBerkeleyMessageConstants.TYPE_NOTICE;
+    }
+
+    /**
+     * delegate to the InternalMessageHandler for now
+     * {@inheritDoc}
+     * 
+     * @see org.sakaiproject.nakamura.api.message.MessageProfileWriter#writeProfileInformation(javax.jcr.Session,
+     *      java.lang.String, org.apache.sling.commons.json.io.JSONWriter)
+     */
+    public void writeProfileInformation(Session session, String recipient, JSONWriter write) {
+        this.messageProfileWriter.writeProfileInformation(session, recipient, write);
     }
 
     protected void bindMessagingService(MessagingService messagingService) {
@@ -123,8 +228,26 @@ public class NoticeHandler implements MessageTransport, MessageProfileWriter {
     protected void unbindSlingRepository(SlingRepository slingRepository) {
         this.slingRepository = null;
     }
+    
+    protected void bindMessageProfileWriter(MessageProfileWriter messageProfileWriter) {
+        this.messageProfileWriter = messageProfileWriter;
+    }
+    
+    protected void unbindMessageProfileWriter(MessageProfileWriter messageProfileWriter) {
+        this.messageProfileWriter = null;
+    }
 
-    public String getType() {
-        return MyBerkeleyMessageConstants.TYPE_NOTICE;
+    protected void activate(ComponentContext context) {
+        Dictionary<?, ?> props = context.getProperties();
+        DateFormat dateFormat = null;
+        String[] dateFormatStrs = OsgiUtil.toStringArray(props.get(PROP_DATE_FORMAT));
+        for (String dateFormatStr : dateFormatStrs) {
+            dateFormat = new SimpleDateFormat(dateFormatStr, Locale.US);
+            this.dateFormats.add(dateFormat);
+        }
+    }
+
+    protected void deactivate(ComponentContext context) {
+        dateFormats = null;
     }
 }
