05325c80ce7b079559bd13867cb9862d59b1ac8d
John King <johnk@media.berkeley.edu>
2010-08-24 14:22:45 -0700

    adding Geobio to project members

--- integration/src/main/scripts/json_data.js
+++ integration/src/main/scripts/json_data.js
@@ -1,9 +1,95 @@
 {
   "users": {
+    "238382": {
+      "lastName": "Geuy",
+      "firstName": "Bernadette",
+      "email": "BernadetteG@berkeley.edu"
+    },
+    "163402": {
+      "lastName": "Scronce",
+      "firstName": "David",
+      "email": "dscronce@berkeley.edu"
+    },
+    "6576": {
+      "lastName": "Christopher",
+      "firstName": "Tony",
+      "email": "tonyc@berkeley.edu"
+    },
+    "592722": {
+      "lastName": "Ellison",
+      "firstName": "Michael"
+    },
+    "3060": {
+      "lastName": "Mcgrath",
+      "firstName": "Owen",
+      "email": "owen@media.berkeley.edu"
+    },
+    "2040": {
+      "lastName": "Heyer",
+      "firstName": "Oliver",
+      "email": "oliver@media.berkeley.edu"
+    },
+    "322279": {
+      "lastName": "Cochran",
+      "firstName": "Eli",
+      "email": "eli@media.berkeley.edu"
+    },
+    "211159": {
+      "lastName": "Davis",
+      "firstName": "Ray",
+      "email": "ray@media.berkeley.edu"
+    },
+    "271592": {
+      "lastName": "King",
+      "firstName": "John f",
+      "email": "johnk@media.berkeley.edu"
+    },
+    "95509": {
+      "lastName": "Chan",
+      "firstName": "Kevin",
+      "email": "kevin@media.berkeley.edu"
+    },
     "758029": {
       "lastName": "Boo",
       "firstName": "Geobio",
       "email": "geobiob@gmail.com"
+    },
+    "750152": {
+      "lastName": "Voytek",
+      "firstName": "Jessica b"
+    },
+    "764968": {
+      "lastName": "Lai",
+      "firstName": "Whitney"
+    },
+    "675750": {
+      "lastName": "Hollowgrass",
+      "firstName": "Rachel",
+      "email": "rhollow@berkeley.edu"
+    },
+    "53791": {
+      "lastName": "Kawase",
+      "firstName": "Darlene",
+      "email": "dkawase@berkeley.edu"
+    },
+    "18437": {
+      "lastName": "German",
+      "firstName": "Greg",
+      "email": "greg@media.berkeley.edu"
+    },
+    "313561": {
+      "lastName": "Waid",
+      "firstName": "Matthew"
+    },
+    "80818": {
+      "lastName": "Hancock",
+      "firstName": "Mara",
+      "email": "mara@media.berkeley.edu"
+    },
+    "760063": {
+      "lastName": "Ng",
+      "firstName": "Nicole",
+      "email": "nicole.ng@berkeley.edu"
     }
   }
 }
