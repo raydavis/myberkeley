5ca65f52c04845874b51ea103868d6d410171be3
Chris Tweney <chris@media.berkeley.edu>
2011-02-11 13:08:05 -0800

    MYB-433 Script to restore production data

---
+++ scripts/restore_data.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# script to restore production data onto your sling instance.
+# this DESTROYS the content repository!
+# more details at:
+# https://confluence.media.berkeley.edu/confluence/display/MYB/Migrating+Production+Data+to+new+Sling+instance
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 source_root sling_password mysql_dump_location jackrabbit_tar_location"
+    exit;
+fi
+
+SRC_LOC=$1
+SLING_PASSWORD=$2
+MYSQL_DUMP=$3
+JACKRABBIT_TAR=$4
+
+MYSQL="mysql -p$SLING_PASSWORD -u sakaiuser"
+
+cd $SRC_LOC/myberkeley
+
+mvn -P runner -Dsling.stop verify
+
+mvn -P runner -Dsling.clean clean
+
+echo "drop database nakamura;" | $MYSQL
+
+echo "create database nakamura default character set 'utf8';" | $MYSQL
+
+mvn -P runner -Dsling.start -Dsling.include.nakamura=$SRC_LOC/launchpad/src/main/resources/nakamura.properties verify
+
+mvn -P runner -Dsling.stop verify
+
+echo "drop database nakamura;" | $MYSQL
+
+echo "create database nakamura default character set 'utf8';" | $MYSQL
+
+gzip -cd $MYSQL_DUMP | $MYSQL
+
+gzip -cd $JACKRABBIT_TAR | tar oxv
+
+./scripts/reinstall.sh $SRC_LOC $SLING_PASSWORD
+
