45de67c6bb0e6c1f31b6609deab87dc14ca77bee
Chris Tweney <chris@media.berkeley.edu>
2011-03-04 09:17:30 -0800

    Send some very rudimentary CalDAV to actual server

--- caldav/src/main/java/edu/berkeley/myberkeley/caldav/CalDavConnector.java
+++ caldav/src/main/java/edu/berkeley/myberkeley/caldav/CalDavConnector.java
@@ -1,4 +1,43 @@
 package edu.berkeley.myberkeley.caldav;
 
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.params.HttpClientParams;
+import org.apache.jackrabbit.webdav.client.methods.OptionsMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class CalDavConnector {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CalDavConnector.class);
+
+    public void getOptions() throws IOException {
+        HttpClient client = new HttpClient();
+
+        HttpState httpState = new HttpState();
+        Credentials credentials = new UsernamePasswordCredentials("vbede", "bedework");
+        AuthScope scope = new AuthScope("test.media.berkeley.edu", 8080);
+        httpState.setCredentials(scope, credentials);
+        client.setState(httpState);
+
+        String uri = "http://test.media.berkeley.edu:8080/ucaldav/";
+        OptionsMethod options = null;
+
+        try {
+            options = new OptionsMethod(uri);
+            client.executeMethod(options);
+            LOGGER.info("OPTIONS method ran on uri " + uri);
+            for ( Header header : options.getResponseHeaders() ) {
+                LOGGER.info(header.getName() + ": " + header.getValue());
+            }
+        } catch ( HttpClientError hce ) {
+            LOGGER.error("Error getting OPTIONS on uri " + uri, hce);
+        } finally {
+            if ( options != null ) {
+                options.releaseConnection();
+            }
+        }
+    }
 }
--- caldavtest/src/test/java/edu/berkeley/myberkeley/caldav/CalDavConnectorTest.java
+++ caldavtest/src/test/java/edu/berkeley/myberkeley/caldav/CalDavConnectorTest.java
@@ -3,12 +3,15 @@ package edu.berkeley.myberkeley.caldav;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class CalDavConnectorTest extends Assert {
 
     @Test
-    public void verifyServer() {
+    public void getOptions() throws IOException {
         CalDavConnector connector = new CalDavConnector();
         assertNotNull(connector);
+        connector.getOptions();
     }
 
 }
