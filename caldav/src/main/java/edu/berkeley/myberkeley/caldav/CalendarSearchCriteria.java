90014fa654ccf152c670958aae80277400598480
Chris Tweney <chris@media.berkeley.edu>
2011-03-28 09:20:16 -0700

    MYB-417 Partial implmentation of required comparator

--- caldav/src/main/java/edu/berkeley/myberkeley/caldav/CalendarSearchCriteria.java
+++ caldav/src/main/java/edu/berkeley/myberkeley/caldav/CalendarSearchCriteria.java
@@ -2,6 +2,7 @@ package edu.berkeley.myberkeley.caldav;
 
 import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.Property;
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.component.VToDo;
 
@@ -24,7 +25,9 @@ public class CalendarSearchCriteria {
     public enum SORT {
 
         DATE_ASC(new DateComparator(true)),
-        DATE_DESC(new DateComparator(false));
+        DATE_DESC(new DateComparator(false)),
+        SUMMARY_ASC(new SummaryComparator(true)),
+        SUMMARY_DESC(new SummaryComparator(false));
 
         private final Comparator<CalendarWrapper> comparator;
 
@@ -62,6 +65,83 @@ public class CalendarSearchCriteria {
                 return -1 * result;
             }
         }
+
+        private static class SummaryComparator implements Comparator<CalendarWrapper> {
+            private final boolean ascending;
+
+            private SummaryComparator(boolean ascending) {
+                this.ascending = ascending;
+            }
+
+            public int compare(CalendarWrapper a, CalendarWrapper b) {
+                int result = 0;
+                Component compA = a.getCalendar().getComponent(Component.VTODO);
+                Component compB = b.getCalendar().getComponent(Component.VTODO);
+                if (compA == null) {
+                    compA = a.getCalendar().getComponent(Component.VEVENT);
+                }
+                if (compB == null) {
+                    compB = b.getCalendar().getComponent(Component.VEVENT);
+                }
+                if (compA != null && compB != null) {
+                    Property summaryA = compA.getProperty(Property.SUMMARY);
+                    Property summaryB = compB.getProperty(Property.SUMMARY);
+                    if (summaryA != null && summaryB != null) {
+                        result = summaryA.getValue().compareTo(summaryB.getValue());
+                    }
+                }
+                if (this.ascending) {
+                    return result;
+                }
+                return -1 * result;
+            }
+        }
+
+        private static class RequiredComparator implements Comparator<CalendarWrapper> {
+            private final boolean ascending;
+
+            private RequiredComparator(boolean ascending) {
+                this.ascending = ascending;
+            }
+
+            public int compare(CalendarWrapper a, CalendarWrapper b) {
+                int result = 0;
+                Component compA = a.getCalendar().getComponent(Component.VTODO);
+                Component compB = b.getCalendar().getComponent(Component.VTODO);
+                if (compA == null) {
+                    compA = a.getCalendar().getComponent(Component.VEVENT);
+                }
+                if (compB == null) {
+                    compB = b.getCalendar().getComponent(Component.VEVENT);
+                }
+                if (compA != null && compB != null) {
+                    Property catsA = compA.getProperty(Property.CATEGORIES);
+                    Property catsB = compB.getProperty(Property.CATEGORIES);
+                    if (catsA == null) {
+                        if (catsB != null && catsB.equals(CalDavConnector.MYBERKELEY_REQUIRED)) {
+                            result = -1;
+                        }
+                    } else {
+
+                    }
+                    if (catsB == null) {
+                        if (catsA != null && catsA.equals(CalDavConnector.MYBERKELEY_REQUIRED)) {
+                            result = 1;
+                        }
+                    }
+                    if (catsA.equals(CalDavConnector.MYBERKELEY_REQUIRED)) {
+
+                    }
+
+                }
+
+                if (this.ascending) {
+                    return result;
+                }
+                return -1 * result;
+            }
+        }
+
     }
 
     private TYPE type;
