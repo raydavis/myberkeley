086bead81fa80c16e5ffa2d88234fb59166acfda
Chris Tweney <chris@media.berkeley.edu>
2011-05-06 14:29:29 -0700

    MYB-572 Fetch our branch first so merge works properly in bamboo machine

--- scripts/automerge.sh
+++ scripts/automerge.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 their_git_url their_branch"
+    echo "Usage: $0 their_git_url their_branch our_git_url our_branch"
     exit;
 fi
 
 THEIR_GIT_URL=$1
 THEIR_BRANCH=$2
-REMOTE_NAME="theirRepo"
+OUR_GIT_URL=$3
+OUR_BRANCH=$4
+
+THEIR_REMOTE_NAME="theirRepo"
+OUR_REMOTE_NAME="ourRepo"
 
 fail() {
-  git remote rm $REMOTE_NAME
+  #git remote rm $THEIR_REMOTE_NAME
   git reset --hard HEAD
   exit 1;
 }
 
-git remote add $REMOTE_NAME $THEIR_GIT_URL
-git fetch $REMOTE_NAME
+git remote add $THEIR_REMOTE_NAME $THEIR_GIT_URL
+git remote add $OUR_REMOTE_NAME $OUR_GIT_URL
+git fetch $THEIR_REMOTE_NAME
+git fetch $OUR_REMOTE_NAME
 
 echo "Git remotes:"
 git remote -v -v
@@ -24,12 +30,8 @@ echo "Git branches:"
 git branch -v -v
 echo
 
-git merge $REMOTE_NAME/$THEIR_BRANCH || { echo "Automatic merge failed. Resetting to HEAD."; fail; }
+git checkout $OUR_REMOTE_NAME/$OUR_BRANCH
+git merge $THEIR_REMOTE_NAME/$THEIR_BRANCH || { echo "Automatic merge failed. Resetting to HEAD."; fail; }
 
-git remote rm $REMOTE_NAME
-
-
-
-
-
-
+#git remote rm $THEIR_REMOTE_NAME
+#git remote rm $OUR_REMOTE_NAME
