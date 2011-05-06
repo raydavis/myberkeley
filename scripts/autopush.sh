e6a8b503b9cb3651ea69c6013dca1c0a0d52a6de
Chris Tweney <chris@media.berkeley.edu>
2011-05-06 15:17:48 -0700

    MYB-572 Fix other bloop in remote name

--- scripts/autopush.sh
+++ scripts/autopush.sh
@@ -30,7 +30,7 @@ echo "Git branches:"
 git branch -v -v
 echo
 
-git checkout -t -b $OUR_REMOTE_NAME/$OUR_BRANCH
+git checkout -t -b $OUR_BRANCH $OUR_REMOTE_NAME/$OUR_BRANCH
 
 echo "Merging $THEIR_REMOTE_NAME/$THEIR_BRANCH..."
 git merge $THEIR_REMOTE_NAME/$THEIR_BRANCH || { echo "Automatic merge failed. Resetting to HEAD."; fail; }
