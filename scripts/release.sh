a4b69fc6e17ba2e8954a2f7357a70c48948626dc
Chris Tweney <chris@media.berkeley.edu>
2011-07-18 14:16:53 -0700

    MYB-730 Update release script to deal with different version numbers for different subparts

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,23 +3,44 @@
 set -o nounset
 set -o errexit
 source_root=$1
-cversion=$2
-nversion=$3
-tag=$4
 
-echo "Creating tagged version: $nversion-$tag"
+nakamura_version=$2
+ian_version=$3
+ux_version=$4
+
+tag=$5
 
 cd $source_root
-
 listofpoms=`find . -name pom.xml -exec grep -l SNAPSHOT {} \;| egrep -v ".git|do_release.sh|target|binary/release|uxloader/src/main/resources|last-release|cachedir"`
-  listofpomswithversion=`grep -l $cversion-SNAPSHOT $listofpoms`
 
-echo "Creating Release"
+echo "-----------------------------------------------------------"
+echo "Creating tagged version of nakamura: $nakamura_version-$tag"
+listofpomswithversion=`grep -l $nakamura_version-SNAPSHOT $listofpoms`
 for i in $listofpomswithversion
 do
-  sed "s/$cversion-SNAPSHOT/$nversion-$tag/" $i > $i.new
+  sed "s/$nakamura_version-SNAPSHOT/$nakamura_version-$tag/" $i > $i.new
   mv $i.new $i
 done
+
+echo "-----------------------------------------------------------"
+echo "Creating tagged version of sparsemap and solr: $ian_version-$tag"
+listofpomswithversion=`grep -l $ian_version-SNAPSHOT $listofpoms`
+for i in $listofpomswithversion
+do
+  sed "s/$ian_version-SNAPSHOT/$ian_version-$tag/" $i > $i.new
+  mv $i.new $i
+done
+
+echo "-----------------------------------------------------------"
+echo "Creating tagged version of ux: $ux_version-$tag"
+listofpomswithversion=`grep -l $ux_version-SNAPSHOT $listofpoms`
+for i in $listofpomswithversion
+do
+  sed "s/$ux_version-SNAPSHOT/$ux_version-$tag/" $i > $i.new
+  mv $i.new $i
+done
+
+echo "-----------------------------------------------------------"
 
 echo "Remaining SNAPSHOT versions in the release"
 echo "=================================================="
