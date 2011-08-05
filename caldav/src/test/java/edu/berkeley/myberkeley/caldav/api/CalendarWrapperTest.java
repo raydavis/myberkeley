0413857b09b73eeef931d2c850f9195261386867
Chris Tweney <chris@media.berkeley.edu>
2011-08-05 16:37:29 -0700

    Revert "Revert 3d994f87965ce4619f12d91cb12f4aa812759c1e (MYB-816 Infrastructure to support the read/unread distinction for notifications)"
    This reverts commit 5bd210232d528b7e92f516f8b2863842e2413851 so that I can fix the failing test.

--- caldav/src/test/java/edu/berkeley/myberkeley/caldav/api/CalendarWrapperTest.java
+++ caldav/src/test/java/edu/berkeley/myberkeley/caldav/api/CalendarWrapperTest.java
@@ -70,6 +70,7 @@ public class CalendarWrapperTest extends CalDavTests {
     assertTrue(wrapper.isCompleted());
     assertTrue(wrapper.isArchived());
     assertTrue(wrapper.isRequired());
+    assertFalse(wrapper.isRead());
   }
 
   @Test
@@ -111,6 +112,7 @@ public class CalendarWrapperTest extends CalDavTests {
     assertNotNull(wrapper.getComponent().getProperty(Property.LOCATION));
     // check for nondestructive deserialization
     assertEquals(wrapper, new CalendarWrapper(wrapper.toJSON()));
+    assertTrue(wrapper.isRead());
   }
 
   @Test
