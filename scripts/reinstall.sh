711f84850b211f4e2e1d699b161e263842d46d5a
Chris Tweney <chris@media.berkeley.edu>
2011-08-08 10:36:46 -0700

    MYB-933 Copy configs from git to working/load every time reinstall runs

--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -13,10 +13,14 @@ INPUT_FILE="$SRC_LOC/.build.cf"
 if [ -f $INPUT_FILE ]; then
   SLING_PASSWORD=`awk -F"=" '/APPLICATION_PASSWORD/ {print $2}' $INPUT_FILE`
   SHARED_SECRET=`awk -F"=" '/SHARED_SECRET/ {print $2}' $INPUT_FILE`
+  CONFIG_FILE_DIR=`awk -F"=" '/CONFIG_FILE_DIR/ {print $2}' $INPUT_FILE`
 else
   SLING_PASSWORD='admin'
   SHARED_SECRET='SHARED_SECRET_CHANGE_ME_IN_PRODUCTION'
+  CONFIG_FILE_DIR="localhost"
 fi
+
+CONFIG_FILES="$SRC_LOC/myberkeley/configs/$CONFIG_FILE_DIR/load"
 
 LOG=$2
 if [ -z "$2" ]; then
@@ -36,14 +40,6 @@ echo "`date`: Cleaning sling directories..." | $LOGIT
 mvn -B -e -P runner -Dsling.purge clean >>$LOG 2>&1 | $LOGIT
 rm -rf ~/.m2/repository/edu/berkeley
 
-# put the shared secret into config file
-SERVER_PROT_CFG=./working/load/org.sakaiproject.nakamura.http.usercontent.ServerProtectionServiceImpl.config
-if [ -f $SERVER_PROT_CFG ]; then
-  grep -v trusted\.secret= $SERVER_PROT_CFG > $SERVER_PROT_CFG.new
-  echo "trusted.secret=\"$SHARED_SECRET\"" >> $SERVER_PROT_CFG.new
-  mv -f $SERVER_PROT_CFG.new $SERVER_PROT_CFG
-fi
-
 echo "`date`: Fetching new sources for myberkeley..." | $LOGIT
 git pull >>$LOG 2>&1
 echo "Last commit:" | $LOGIT
@@ -60,6 +56,19 @@ echo | $LOGIT
 echo "------------------------------------------" | $LOGIT
 
 cd ../myberkeley
+
+echo "Updating local configuration files..." | $LOGIT
+
+# put the shared secret into config file
+SERVER_PROT_CFG=$CONFIG_FILES/org.sakaiproject.nakamura.http.usercontent.ServerProtectionServiceImpl.config
+if [ -f $SERVER_PROT_CFG ]; then
+  grep -v trusted\.secret= $SERVER_PROT_CFG > $SERVER_PROT_CFG.new
+  echo "trusted.secret=\"$SHARED_SECRET\"" >> $SERVER_PROT_CFG.new
+  mv -f $SERVER_PROT_CFG.new $SERVER_PROT_CFG
+fi
+
+rm $SRC_LOC/myberkeley/working/load/*
+cp -f $CONFIG_FILES/* $SRC_LOC/myberkeley/working/load
 
 echo "`date`: Doing clean install..." | $LOGIT
 mvn -B -e clean install >>$LOG 2>&1
