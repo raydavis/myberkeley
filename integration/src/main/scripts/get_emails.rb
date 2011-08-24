ba98a0685b169ce539943c2a44164cf6e7c40887
Ray Davis <ray@media.berkeley.edu>
2011-08-24 14:06:21 -0700

    MYB-1019 Script to pull email lists from profiles and demographics

--- integration/src/main/scripts/get_emails.rb
+++ integration/src/main/scripts/get_emails.rb
@@ -14,19 +14,57 @@ require 'ucb_data_loader'
 @log.level = Logger::DEBUG
 
 def output_email(sling, username)
-  res = sling.execute_get(sling.url_for("~#{username}/public/authprofile/email.2.json"))
+  puts "#{email}\n"
+end
+
+def collect_accounts(sling, username, lists)
+  res = sling.execute_get(sling.url_for("~#{username}/_myberkeley-demographic.2.json"))
   if (res.code.to_i > 299)
     @log.error("#{res.code} / #{res.body}")
     return
   end
   json = JSON.parse(res.body)
-  email = json["elements"]["email"]["value"]
-  puts "#{email}\n"
+  demographics = json["myb-demographics"]
+  colleges = []
+  rex = /^\/colleges\/([A-Za-z0-9_\s]+)\//
+  demographics.each do |demographic|
+    matches = rex.match(demographic)
+    if (matches)
+      newcollege = matches[1]
+      if !colleges.include?(newcollege)
+        colleges.push(newcollege)
+      end
+    end
+  end
+  res = sling.execute_get(sling.url_for("~#{username}/public/authprofile.profile.4.json"))
+  if (res.code.to_i > 299)
+    @log.error("#{res.code} / #{res.body}")
+    return
+  end
+  profile = JSON.parse(res.body)
+  emailSection = profile["email"]
+  if (!emailSection.nil? && !emailSection["elements"].nil? && !profile["institutional"].nil?)
+    email = emailSection["elements"]["email"]["value"]
+    colleges.each do |college|
+      if (lists[college].nil?)
+        lists[college] = []
+      end
+      lists[college].push(email)
+    end
+  end
 end
 
 @ucb_data_loader = MyBerkeleyData::UcbDataLoader.new(ARGV[0], ARGV[1])
 @sling = @ucb_data_loader.sling
 remaining_accounts = @ucb_data_loader.get_all_ucb_accounts
+email_lists = {}
 remaining_accounts.each do |account_uid|
-	output_email(@sling, account_uid)
+  collect_accounts(@sling, account_uid, email_lists)
+end
+email_lists.each do |college_name, emails|
+  puts "#{college_name}\n"
+  emails.each do |email|
+    puts "#{email}\n"
+  end
+  puts "\n"
 end
