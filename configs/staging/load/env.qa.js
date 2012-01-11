a544f5dde2462b28f6c99ff64e800e349b3b8d98
Ray Davis <ray@media.berkeley.edu>
2012-01-11 09:51:46 -0800

    MYB-1286 Add staging config

---
+++ configs/staging/load/env.qa.js
@@ -0,0 +1,11 @@
+define(["config/config"], function(config) {
+    config.isDev = false;
+	
+    // Tracking and statistics
+    config.Tracking = {
+        GoogleAnalytics: {
+            WebPropertyID : "UA-21809393-2"
+        }
+    };
+	
+});
