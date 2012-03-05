2ff56ffff9176aa17ea86a22a95d7000fc09ad72
Ray Davis <ray@media.berkeley.edu>
2012-03-05 14:46:33 -0800

    MYB-1472 Delay integration test until server has finished start-up

--- scripts/myberkeley_integration.sh
+++ scripts/myberkeley_integration.sh
@@ -26,11 +26,21 @@ echo "trusted.hosts=[\"localhost:$PORT\\ \\=\\ http://localhost:8082\"]" > worki
 # start up
 mvn -P runner -Dsling.start -Dsling.port=$PORT verify || { echo "Failed to start sling." ; stopAndExit ; }
 
-# wait 2 minutes so sling can get going
-sleep 120;
+# wait so sling can get going
+echo "Waiting for startup..."
+MAXTRIES=10
+TRIES=0
+LOGFILE=working/sling/logs/error.log
+while ! [ $TRIES -gt $MAXTRIES ] && ! [ -e $LOGFILE ] ;do sleep 30; TRIES=$((TRIES+1)); done
+while ! [ $TRIES -gt $MAXTRIES ] && ! grep "org.sakaiproject.nakamura.world BundleEvent STARTED" $LOGFILE > /dev/null  2>&1;do sleep 30; TRIES=$((TRIES+1)); done
+if [ $TRIES -gt $MAXTRIES ]
+then
+    echo "Startup did not complete for a long time. Giving up."
+    exit 1
+fi
 
 # run myberkeley dataloader
-mvn -Dsling.loaddata -Dsling.port=$PORT -Dloaddata.server=http://localhost:$PORT/ -Dloaddata.password=admin integration-test || { echo "Integration-test failed." ; stopAndExit ; }
+mvn -e -P runner -Dsling.loaddata -Dsling.port=$PORT -Dloaddata.server=http://localhost:$PORT/ -Dloaddata.password=admin integration-test || { echo "Integration-test failed." ; stopAndExit ; }
 
 # stop server
 mvn -P runner -Dsling.stop -Dsling.port=$PORT verify
